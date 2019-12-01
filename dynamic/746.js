2cc9eec24a2b023d20cfef218f3a709dacfc73af
jayjliang <1948448114@qq.com>
2019-12-01 21:56:24 +0800

    add cv

---
+++ dynamic/746.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function (cost) {
+    let dp = [cost[0],cost[1]];
+    for(let i = 2; i < cost.length; i++) {
+        dp[i] = Math.min(dp[i-1], dp[i-2]) + cost[i];
+    }
+    return Math.min(dp[cost.length - 1], dp[cost.length - 2]);
+};
+
+console.log(minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]))
